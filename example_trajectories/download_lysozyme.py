4cc4122e8fb07a27d647bec185701837920579f4
padix-key <patrick.kunzm@gmail.com>
2019-02-27 13:57:30 +0100

    Fixed path

--- example_trajectories/download_lysozyme.py
+++ example_trajectories/download_lysozyme.py
@@ -1,6 +1,6 @@
-from os.path import dirname
+from os.path import dirname, abspath
 import os
 import biotite.database.rcsb as rcsb
 
-file_name = rcsb.fetch("1aki", "pdb", dirname(snakemake.output[0]))
+file_name = rcsb.fetch("1aki", "pdb", dirname(abspath(snakemake.output[0])))
 os.rename(file_name, snakemake.output[0])
